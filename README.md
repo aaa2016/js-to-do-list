82e1a8103eacf7b1bdab57e332b4a8021c665802
aaa2016 <artur.arikainen@gmail.com>
2020-02-05 23:50:30 +0000

    Added main list functionality (saving to cookie still WIP)

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # JavaScript To Do List
 
-Work in progress
+*Work in progress*
+
+##Current functionality:
+
+1. Add items to list
+2. Delete individual items
+3. Delete all items
+
+##Future functionality:
+
+1. Save list when closing browser using cookies
+2. Retrieve list from cookie when reopening browser
--- todolist.js
+++ todolist.js
@@ -1,9 +1,11 @@
 var items = [];
 
 function checkCookie() {
-	if (document.cookie != "") {
-		window.alert("Cookie is: "+document.cookie);
-		items = document.cookie;  // TODO: select correct cookie
+	if (getCookie("items") != "") {
+		//window.alert("Cookie is: "+document.cookie);
+		itemsString = getCookie("items");
+		//window.alert("Items in cookie are: "+itemsString);
+		items = itemsString.split(",");
 		drawList(items);
 	}
 }
@@ -18,7 +20,7 @@ function addItem() {
 		// add to array of items
 		items.push(item);
 	
-		// set cookie
+		// set cookie - TODO properly
 		//setCookie(items);
 	
 		//draw list of items
@@ -26,49 +28,68 @@ function addItem() {
 	
 		//clear input field, and put cursor back in
 		document.getElementById("inputItem").value = "";
-		document.getElementById("inputItem").focus();
 	
 	}
+	
+	document.getElementById("inputItem").focus();
 
 }
 
 function drawList(input) {
 	
+	//window.alert("Drawing list from: "+input);
+	
 	// for each element in the array, create a new p element
 	for (i=0; i<input.length; i++) {
-		var para = document.createElement("p");
-		para.setAttribute("id","item")
+		//window.alert("Input i: "+i+" is "+input[i]);
+		var p = document.createElement("p");
+		p.setAttribute("id","item"+i);
+		p.setAttribute("class","list-item");
 		var node = document.createTextNode(input[i]);
-		para.appendChild(node);
-		var element = document.getElementById("list-area");
+		p.appendChild(node);
+		
+		var delLink = document.createElement("a");
+		var linkText = document.createTextNode("X");
+		delLink.setAttribute("href","#");
+		delLink.setAttribute("onclick", "removeItem('item"+i+"'); return false");
+		delLink.appendChild(linkText);
+		p.appendChild(delLink);
+		
+		// TODO: fix - when using cookie, only draws last item
+		
 	}
 	
-	// TODO: create delete item link for each item
-	
 	// add elements to list area
-	element.appendChild(para);
+	var listArea = document.getElementById("list-area");
+	listArea.appendChild(p); 
 	
 }
 
-function removeItem() {
+function removeItem(n) {
 
-	//TODO: create this
+	var itemToDelete = document.getElementById(n);
+	
+	while (itemToDelete.firstChild) {
+    	itemToDelete.removeChild(itemToDelete.firstChild);
+  	}
+  	
+  	// TODO: delete entry in cookie
 
 }
 
 function deleteAll() {
 
 	// clear the list area
-	var list = document.getElementById("list-area");
-	for (i=0; i<list.childElementCount; i++) {
-		list.removeChild(list.childNodes[i]);
-	}
+	var listArea = document.getElementById("list-area");
+  	while (listArea.firstChild) {
+    	listArea.removeChild(listArea.firstChild);
+  	}
 	
 	// clear array
 	items = [];
 	
 	// delete cookie
-	//deleteCookie();
+	deleteCookie();
 
 }
 
@@ -77,6 +98,23 @@ function setCookie(input) {
 	// set cookie expiry date to maximum future date (32 bit)
 	document.cookie = "items="+input+";expires=Tue, 19 Jan 2038 03:14:07 UTC; path=/";
 
+}
+
+function getCookie(cname) {
+	// This is borrowed from W3C Schools: https://www.w3schools.com/js/js_cookies.asp
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+    	var c = ca[i];
+    	while (c.charAt(0) == ' ') {
+    	c = c.substring(1);
+    	}
+    	if (c.indexOf(name) == 0) {
+      		return c.substring(name.length, c.length);
+    	}
+  	}
+  return "";
 }
 
 function deleteCookie() {
